a9955bfade6462b05608b9d96f5ff6ed2fda4d97
Nishit Patel <nishit.patel@morningstar.com>
2024-04-18 00:50:58 +0530

    refactor: esm module, fix tests

--- test.mjs
+++ test.mjs
@@ -1,20 +1,27 @@
-import should from 'should';
+import 'should';
 import jsdom from 'jsdom';
+import { describe, it } from 'mocha';
 
 describe('canUseDOM', function () {
-  it('should return false in nodejs environment', function (done) {
-    var canUseDOM = require('./index');
-    canUseDOM.should.be.false;
-    done();
-  });
+    it('should return false in nodejs environment', async () => {
+        const module = await import('./index.mjs');
 
-  // it('should return true in browser', function (done) {
-  //   jsdom.env('<div></div>', function (err, window) {
-  //     global.document = jsdom.jsdom('');
-  //     global.window = document.defaultView;
-  //     var canUseDOM = require('./index');
-  //     canUseDOM.should.be.true;
-  //     done();
-  //   });
-  // });
+        const canUseDOM = module.default;
+
+        canUseDOM.should.be.false;
+    });
+
+    it('should return true in browser', async () => {
+        const { JSDOM } = jsdom;
+        const { window } = new JSDOM('<!doctype html><html><body></body></html>');
+
+        global.document = window.document;
+        global.window = window;
+
+        const module = await import('./index.mjs');
+
+        const canUseDOM = module.default;
+
+        canUseDOM.should.be.true;
+    });
 });
